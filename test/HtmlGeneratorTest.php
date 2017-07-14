987fbf97ea16326fdea90371ef3eadd7ee22babd
Hermann Krumrey <hermann@krumreyh.com>
2017-07-14 11:01:03 +0200

    Create first source files

---
+++ test/HtmlGeneratorTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright Hermann Krumrey <hermann@krumreyh.com> 2017
+ *
+ * This file is part of champlates.
+ *
+ * champlates is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * champlates is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with champlates.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * Class HtmlGeneratorTest
+ * Tests the generation of various HTML Elements
+ */
+class HtmlGeneratorTest extends TestCase {
+
+	/**
+	 * Placeholder test
+	 */
+	public function test() {
+		$this->assertTrue(true);
+	}
+	
+}
