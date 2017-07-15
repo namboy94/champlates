0767a9b59881801ba7c3d83ed39effe503bad8ed
Hermann Krumrey <hermann@krumreyh.com>
2017-07-15 22:19:09 +0200

    Add div element

---
+++ src/widgets/Div.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hermann
+ * Date: 7/15/17
+ * Time: 10:11 PM
+ */
+
+namespace chameleon_widgets;
+use chameleon\HtmlElement;
+
+
+/**
+ * Class Div
+ * A Simple Div element
+ * @package chameleon_widgets
+ */
+class Div extends HtmlElement {
+
+	/**
+	 * Div constructor.
+	 * @param array $content: The content inside the div
+	 * @param array $classes: The classes of the div, as an array of strings
+	 */
+	public function __construct(array $content, array $classes) {
+		parent::__construct(__DIR__ . "/templates/div.html", null);
+
+		$classDefinition = "";
+		foreach ($classes as $class) {
+			$classDefinition .= $class . " ";
+		}
+		$this->bindParam("CLASS", $classDefinition);
+		$this->addCollectionFromArray("CONTENT", $content);
+	}
+
+}
