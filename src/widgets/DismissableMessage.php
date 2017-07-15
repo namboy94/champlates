ae4fe8c6295a13b70d0fb4f4821765e4627a059e
Hermann Krumrey <hermann@krumreyh.com>
2017-07-15 02:15:09 +0200

    Adjust license headers

--- src/widgets/DismissableMessage.php
+++ src/widgets/DismissableMessage.php
@@ -1,9 +1,21 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: hermann
- * Date: 7/15/17
- * Time: 1:21 AM
+ * Copyright Hermann Krumrey <hermann@krumreyh.com> 2017
+ *
+ * This file is part of champlates.
+ *
+ * champlates is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * champlates is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with champlates.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace chameleon_widgets;
