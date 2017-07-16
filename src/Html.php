49060b2a66702a9d7f7cd4a630d89f4da687aad8
Hermann Krumrey <hermann@krumreyh.com>
2017-07-17 00:50:24 +0200

    Add Html class

---
+++ src/Html.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright Hermann Krumrey <hermann@krumreyh.com> 2017
+ *
+ * This file is part of champlates.
+ *
+ * champlates is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * champlates is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with champlates.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace chameleon;
+
+
+/**
+ * Class Html
+ * A Very Simple class that allows rendering an HTML snippet
+ * @package chameleon
+ */
+class Html extends HtmlTemplate {
+
+	/**
+	 * Html constructor.
+	 * @param string $html: The HTML to render
+	 * @param Dictionary|null $dict: Optinal dictionary
+	 */
+	public function __construct(string $html, ? Dictionary $dict = null) {
+		parent::__construct(__DIR__ . "/templates/html.html", $dict);
+		$this->bindParam("HTML", $html);
+	}
+
+}
---
+++ test/HtmlTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright Hermann Krumrey <hermann@krumreyh.com> 2017
+ *
+ * This file is part of champlates.
+ *
+ * champlates is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * champlates is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with champlates.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use chameleon\Html;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * Class HtmlTest
+ * Tests the Html class
+ */
+class HtmlTest extends TestCase {
+
+	/**
+	 * Tests a simple render of an html snippet
+	 */
+	public function testHtmlRendering() {
+		$html = new Html("<h1>Hello</h1>");
+		$this->assertEquals($html->render(""), "<h1>Hello</h1>");
+	}
+
+}
